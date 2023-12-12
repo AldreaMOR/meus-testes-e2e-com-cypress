99c03e0368d89281452d8b2c5d77d7d3f5407afe
Aldrea Rabelo <aldrea.mno@gmail.com>
2023-12-12 19:50:16 -0300

    Corrigindo configuração com o mailosaur

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  requestTimeout: 10000,
   e2e: {
     baseUrl:'https://notes-serverless-app.com',
     setupNodeEvents(on, config) {
